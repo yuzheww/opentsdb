de53a81bfaf566354d75b221e1f1baef57ae207a
Sean P. Miller <spmiller@verizonmedia.com>
2021-09-06 02:40:45 +0000

    Add config param to force FP div.

--- core/src/main/java/net/opentsdb/query/processor/expressions2/EvaluationOptions.java
+++ core/src/main/java/net/opentsdb/query/processor/expressions2/EvaluationOptions.java
@@ -3,13 +3,20 @@ package net.opentsdb.query.processor.expressions2;
 public class EvaluationOptions {
     public static class Builder {
         private boolean infectiousNaN;
+        private boolean useFloatingPointDivision;
 
         public Builder() {
             infectiousNaN = false;
+            useFloatingPointDivision = true;
         }
 
         public Builder setInfectiousNaN(final boolean flag) {
             this.infectiousNaN = flag;
+            return this;
+        }
+
+        public Builder setUseFloatingPointDivision(final boolean flag) {
+            this.useFloatingPointDivision = flag;
             return this;
         }
 
@@ -19,12 +26,18 @@ public class EvaluationOptions {
     }
 
     private final boolean infectiousNaN;
+    private final boolean useFloatingPointDivision;
 
     private EvaluationOptions(final Builder builder) {
         this.infectiousNaN = builder.infectiousNaN;
+        this.useFloatingPointDivision = builder.useFloatingPointDivision;
     }
 
     public boolean getInfectiousNaN() {
         return infectiousNaN;
+    }
+
+    public boolean getUseFloatingPointDivision() {
+        return useFloatingPointDivision;
     }
 }
